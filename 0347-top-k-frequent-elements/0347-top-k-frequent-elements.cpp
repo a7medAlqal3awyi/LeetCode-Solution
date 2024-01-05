b42ea528f616dde5f3d6cd90bb03b3a0da6acf33
Ahmed Khaled Alkalawyi <100479326+a7medAlqal3awyi@users.noreply.github.com>
2024-01-05 22:27:11 +0200

    Time: 8 ms (91.20%), Space: 13.9 MB (97.67%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+   unordered_map<int, int> frequency_map;
+
+    for (int num : nums) {
+        frequency_map[num]++;
+    }
+
+    vector<int> result;
+    for (const auto& entry : frequency_map) {
+        result.push_back(entry.first);
+    }
+
+    partial_sort(result.begin(), result.begin() + k, result.end(),
+                 [&](int a, int b) {
+                     return frequency_map[a] > frequency_map[b]; 
+                 });
+
+    result.resize(k);
+
+    return result;
+}
+
+};
