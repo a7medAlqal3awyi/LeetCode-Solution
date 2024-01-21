83bf4e47b060d94fb86fd58bc14da0987f1e7bd6
Ahmed Khaled Alkalawyi <100479326+a7medAlqal3awyi@users.noreply.github.com>
2024-01-21 02:38:50 +0200

    Time: 24 ms (89.34%), Space: 23.4 MB (15.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-    unordered_map<string, vector<string>> anagramGroups;
+    unordered_map< string, vector<string>> anagramGroups;
     
     for (const string& str : strs) {
+        
         string sortedStr = str;
         sort(sortedStr.begin(), sortedStr.end());
-        
         anagramGroups[sortedStr].push_back(str);
     }
     
